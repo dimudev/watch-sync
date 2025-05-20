a59f6b5c2ef66493a124738f4beb015955263a19
dimudev <dimudeveloper@gmail.com@>
2025-05-20 16:53:35 -0500

    add AppHeader component in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 'use client'
-import { useSocketStore } from '@/store/socketStore'
+import AppHeader from '@/components/global/base/header/AppHeader'
 import React from 'react'
 
 
 const Home = () => {
-  const online = useSocketStore((state) => state.online)
-  const clientsConnected = useSocketStore((state) => state.clientsConnected)
   return (
-    <div>
-      <h1>Online: {online ? 'Yes' : 'No'}</h1>
-      <h1>Online: {clientsConnected}</h1>
-    </div>
+    <>
+      <AppHeader/>
+    </>
   )
 }
 
